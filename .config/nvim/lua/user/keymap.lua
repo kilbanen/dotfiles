0cf6b23e10e935fad7acde3f974849a00570a112
Kilbane <kilbanen@tcd.ie>
2022-04-27 22:03:00 +0100

    Update keymap.lua to use vim.keymap.set

--- .config/nvim/lua/user/keymap.lua
+++ .config/nvim/lua/user/keymap.lua
@@ -1,34 +1,33 @@
-local opts = {noremap = true, silent = true}
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 
-keymap("n", "<Space>", "<Nop>", opts)
+keymap("n", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Switch window
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h")
+keymap("n", "<C-j>", "<C-w>j")
+keymap("n", "<C-k>", "<C-w>k")
+keymap("n", "<C-l>", "<C-w>l")
 
 -- Leader key
-keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+keymap("n", "<leader>e", ":Lex 30<cr>")
 
 -- Switch buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>")
+keymap("n", "<S-h>", ":bprevious<CR>")
 
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv")
+keymap("v", ">", ">gv")
 
 -- Move text
-keymap("n", "<A-j>", ":m .+1<CR>==", opts)
-keymap("n", "<A-k>", ":m .-2<CR>==", opts)
-keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
-keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
-keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+keymap("n", "<A-j>", ":m .+1<CR>==")
+keymap("n", "<A-k>", ":m .-2<CR>==")
+keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
+keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv")
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv")
 
 -- Don't yank replaced text after pasting in visual mode
-keymap("v", "p", '"_dP', opts)
+keymap("v", "p", '"_dP')
